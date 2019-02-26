2db6ab6aafeed828e390991b88c0894eb71f5b0b
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-26 23:09:02 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,6 @@ class ProductsController < ApplicationController
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:id, :name, :description, :inventory, :price]
     end
-    end
   end
 
   def data
